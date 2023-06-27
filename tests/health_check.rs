0537ac2da0e3de480b54112e57ab3444d021203e
brvdyy <brvdy.dev@gmail.com>
2023-06-26 23:54:50 -0500

    fixed formatting to test workflow

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,10 +5,10 @@ use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+        .expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
 
-    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
@@ -34,7 +34,7 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data () {
     //Arrange
-    let configuration = get_configuration().expect("Failed to read configuration");
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let app_address = spawn_app();
     let connection_string = configuration.database.connection_string();
     let mut connection = PgConnection::connect(&connection_string)
@@ -58,7 +58,7 @@ async fn subscribe_returns_200_for_valid_form_data () {
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
         .fetch_one(&mut connection)
         .await
-        .expect("Failed to fetch saved subscriptions");
+        .expect("Failed to fetch saved subscriptions.");
 
     assert_eq!(saved.email, "brvdy.dev&gmail.com");
     assert_eq!(saved.name, "brady carmichael");
@@ -83,12 +83,12 @@ async fn subscribe_returns_400_when_data_is_missing() {
             .body(invalid_body)
             .send()
             .await
-            .expect("Failed to execute requet");
+            .expect("Failed to execute requet.");
 
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}", error_message
+            "The API did not fail with 400 Bad Request when the payload was {}.", error_message
         );
     }
 }
